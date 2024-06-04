77d96b48465b27f14e512261eec77ca3a210d8d5
RubenRamR <160176918+RubenRamR@users.noreply.github.com>
2024-06-04 16:44:11 -0700

    Creación y pom

---
+++ ProyectoCinepolis/src/main/java/com/mycompany/proyectocinepolis/ProyectoCinepolis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocinepolis;
+
+/**
+ *
+ * @author rramirez
+ */
+public class ProyectoCinepolis {
+
+    public static void main(String[] args) {
+
+    }
+}
