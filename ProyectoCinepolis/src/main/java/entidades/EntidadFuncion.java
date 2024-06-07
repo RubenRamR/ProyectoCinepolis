51fba6a9523edf40d9a097c0aa998a47c6d11ba9
DavidElierCampaChaparro <141787493+davidelierspecter@users.noreply.github.com>
2024-06-06 18:18:59 -0700

    Entidades y relaciones hechas

---
+++ ProyectoCinepolis/src/main/java/entidades/EntidadFuncion.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author crazy
+ */
+public class EntidadFuncion {
+    int id;
+    float precio;
+    Timestamp inicio;
+    Timestamp fin;
+    Time tiempoLimpieza;
+    int idPelicula;
+    int idSala;
+    
+    public EntidadFuncion() {
+    }
+    
+    // constructor con todos los atributos
+    public EntidadFuncion(int id, float precio, Timestamp inicio, Timestamp fin, Time tiempoLimpieza, int idPelicula, int idSala) {
+        this.id = id;
+        this.precio = precio;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.tiempoLimpieza = tiempoLimpieza;
+        this.idPelicula = idPelicula;
+        this.idSala = idSala;
+    }
+    
+    // Constructor sin id
+    public EntidadFuncion(float precio, Timestamp inicio, Timestamp fin, Time tiempoLimpieza, int idPelicula, int idSala) {
+        this.precio = precio;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.tiempoLimpieza = tiempoLimpieza;
+        this.idPelicula = idPelicula;
+        this.idSala = idSala;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Timestamp getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Timestamp inicio) {
+        this.inicio = inicio;
+    }
+
+    public Timestamp getFin() {
+        return fin;
+    }
+
+    public void setFin(Timestamp fin) {
+        this.fin = fin;
+    }
+
+    public Time getTiempoLimpieza() {
+        return tiempoLimpieza;
+    }
+
+    public void setTiempoLimpieza(Time tiempoLimpieza) {
+        this.tiempoLimpieza = tiempoLimpieza;
+    }
+
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public int getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(int idSala) {
+        this.idSala = idSala;
+    }
+    
+    
+    
+    
+}
---
+++ ProyectoCinepolis/src/main/java/entidades/EntidadPelicula.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.sql.Time;
+
+/**
+ *
+ * @author crazy
+ */
+public class EntidadPelicula {
+    int id;
+    String titulo;
+    String genero;
+    String clasificacion;
+    String sinopsis;
+    Time duracion;
+    String paisOrigen;
+    String trailerLink;
+    
+    public EntidadPelicula() {
+    }
+    
+    // constructor con todos los atributos
+    public EntidadPelicula(int id, String titulo, String genero, String clasificacion, String sinopsis, Time duracion, String paisOrigen, String trailerLink) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.clasificacion = clasificacion;
+        this.sinopsis = sinopsis;
+        this.duracion = duracion;
+        this.paisOrigen = paisOrigen;
+        this.trailerLink = trailerLink;
+    }
+
+    // constructor sin ID
+    public EntidadPelicula(String titulo, String genero, String clasificacion, String sinopsis, Time duracion, String paisOrigen, String trailerLink) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.clasificacion = clasificacion;
+        this.sinopsis = sinopsis;
+        this.duracion = duracion;
+        this.paisOrigen = paisOrigen;
+        this.trailerLink = trailerLink;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public Time getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Time duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public void setPaisOrigen(String paisOrigen) {
+        this.paisOrigen = paisOrigen;
+    }
+
+    public String getTrailerLink() {
+        return trailerLink;
+    }
+
+    public void setTrailerLink(String trailerLink) {
+        this.trailerLink = trailerLink;
+    }
+ 
+}
