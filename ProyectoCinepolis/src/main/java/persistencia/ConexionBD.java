5a7c4ad81cd992cf4be8ea0e5fa5d7a9263a0830
DavidElierCampaChaparro <141787493+davidelierspecter@users.noreply.github.com>
2024-06-06 18:24:53 -0700

    persistencia paquete
    agregué el paquete persistencia y la copié del crudAlumnos la persistenciaexception y la conexion a la bd

---
+++ ProyectoCinepolis/src/main/java/persistencia/ConexionBD.java
@@ -0,0 +1,21 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConexionBD implements IConexionBD {
+    
+    final String SERVER = "localhost";
+    final String BASE_DATOS = "clase";
+    private final String CADENA_CONEXION = "jdbc:mysql://" + SERVER + "/" + BASE_DATOS;
+    final String USUARIO = "root";
+    final String CONTRASEÑA = "xr471112";
+    
+    @Override
+    public Connection crearConexion() throws SQLException {
+        Connection conexion = DriverManager.getConnection(CADENA_CONEXION, USUARIO, CONTRASEÑA);
+        return conexion;   
+    }
+}
